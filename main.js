d3f5c77ab308c2d38f82f0dcd050c2dd4ad34821
Aleksandr GUssev <gussev.sanya@hotmail.com>
2025-04-27 18:34:25 +0300

    changed logic

--- main.js
+++ main.js
@@ -2,31 +2,31 @@
 const teamData = [
   {
     name: 'Chris Ellis',
-    bio: 'Chris Ellis founded Bijou Wine in 2013 after twenty-five years in the wine industry. Previously he has worked as Export Director for a large French wine producer and was a founder shareholder of Western Wines. He has also consulted to a number of vineyards, wine producers, distributors and retailers. Chris was at the forefront of the retail wine revolution and has been heavily involved in both production and sales and marketing. He created, sourced and developed brands such as Kumala and Isla Negra and was the first person to bring in brands such as Cono Sur to the European Market.'
+    text: 'Chris Ellis founded Bijou Wine in 2013 after twenty-five years in the wine industry. Previously he has worked as Export Director for a large French wine producer and was a founder shareholder of Western Wines. He has also consulted to a number of vineyards, wine producers, distributors and retailers. Chris was at the forefront of the retail wine revolution and has been heavily involved in both production and sales and marketing. He created, sourced and developed brands such as Kumala and Isla Negra and was the first person to bring in brands such as Cono Sur to the European Market.'
   },
   {
     name: 'Edward Vellacott',
-    bio: 'English by birth, Edward was brought up in the South of France from the age of nine. After completing his French Baccalaureate he returned to the UK to study Management and Spanish at the University of Leeds, the highlight of which was spending a year working in the export division of a Spanish winery in Madrid.  Edward’s trustworthy personality allied with his interpersonal and communication skills continuously help him build strong cross-cultural relationships. He passed WSET Level 3 with Merit and will shortly complete the Diploma course.'
+    text: 'English by birth, Edward was brought up in the South of France from the age of nine. After completing his French Baccalaureate he returned to the UK to study Management and Spanish at the University of Leeds, the highlight of which was spending a year working in the export division of a Spanish winery in Madrid.  Edward’s trustworthy personality allied with his interpersonal and communication skills continuously help him build strong cross-cultural relationships. He passed WSET Level 3 with Merit and will shortly complete the Diploma course.'
   },
   {
     name: 'Emily West',
-    bio: 'Emily has over a decade of experience in marketing and brand development within agencies, as a consultant and in-house. She has previously planned and executed the marketing strategy for a new global skin clinic, curated art galleries and worked as an art dealer internationally, and has consulted on marketing and culture for brands such as MTV, Post Office, Casio and Kit Heath. Emily enjoys photography and has shot with brands such as DeBeers, Lofbergs and Charity Bank. She is training for her WSET and, when not working, enjoys walks with her miniature dachshund, Mayer (a nod to legendary guitarist, John).'
+    text: 'Emily has over a decade of experience in marketing and brand development within agencies, as a consultant and in-house. She has previously planned and executed the marketing strategy for a new global skin clinic, curated art galleries and worked as an art dealer internationally, and has consulted on marketing and culture for brands such as MTV, Post Office, Casio and Kit Heath. Emily enjoys photography and has shot with brands such as DeBeers, Lofbergs and Charity Bank. She is training for her WSET and, when not working, enjoys walks with her miniature dachshund, Mayer (a nod to legendary guitarist, John).'
   },
   {
     name: 'Hélène Temmerman',
-    bio: 'Hélène was born and raised in Belgium where she graduated as a bio engineer specializing in Agronomy. To pursue her passion for wine she has worked in countries such as Italy, France, and Croatia to research sustainability practices in the wine sector and to gain practical experience. Her additional Master’s in International Food and Beverage management at the business school of ESCP in Turin and Paris and her previous experience as business manager gave her in-depth knowledge of the commercial aspects of the wine market. Hélène has completed her WSET diploma.'
+    text: 'Hélène was born and raised in Belgium where she graduated as a bio engineer specializing in Agronomy. To pursue her passion for wine she has worked in countries such as Italy, France, and Croatia to research sustainability practices in the wine sector and to gain practical experience. Her additional Master’s in International Food and Beverage management at the business school of ESCP in Turin and Paris and her previous experience as business manager gave her in-depth knowledge of the commercial aspects of the wine market. Hélène has completed her WSET diploma.'
   },
   {
     name: 'Maria Giulia Alicino',
-    bio: ''
+    text: 'Emily has over a decade of experience in marketing and brand development within agencies, as a consultant and in-house. She has previously planned and executed the marketing strategy for a new global skin clinic, curated art galleries and worked as an art dealer internationally, and has consulted on marketing and culture for brands such as MTV, Post Office, Casio and Kit Heath. Emily enjoys photography and has shot with brands such as DeBeers, Lofbergs and Charity Bank. She is training for her WSET and, when not working, enjoys walks with her miniature dachshund, Mayer (a nod to legendary guitarist, John).'
   },
   {
     name: 'Amber Baker',
-    bio: ''
+    text: 'Emily has over a decade of experience in marketing and brand development within agencies, as a consultant and in-house. She has previously planned and executed the marketing strategy for a new global skin clinic, curated art galleries and worked as an art dealer internationally, and has consulted on marketing and culture for brands such as MTV, Post Office, Casio and Kit Heath. Emily enjoys photography and has shot with brands such as DeBeers, Lofbergs and Charity Bank. She is training for her WSET and, when not working, enjoys walks with her miniature dachshund, Mayer (a nod to legendary guitarist, John).'
   },
   {
     name: 'Helen Day',
-    bio: ''
+    text: 'Helen [ssl][dl[s[ds[d]]]] has over a decade of experience in marketing and brand development within agencies, as a consultant and in-house. She has previously planned and executed the marketing strategy for a new global skin clinic, curated art galleries and worked as an art dealer internationally, and has consulted on marketing and culture for brands such as MTV, Post Office, Casio and Kit Heath. Emily enjoys photography and has shot with brands such as DeBeers, Lofbergs and Charity Bank. She is training for her WSET and, when not working, enjoys walks with her miniature dachshund, Mayer (a nod to legendary guitarist, John).'
   },
 ];
 
@@ -88,10 +88,10 @@ function unlockBody() {
 
 
 
+let sideBarWidthDesktop = 600;
 
 
-
-let sidebarWidth = window.innerWidth <= 992 ? '100%' : 700
+let sidebarWidth = window.innerWidth <= 640 ? '100%' : sideBarWidthDesktop
 $(sidebar).slideReveal({
   trigger: $('.team__item'),
   position: 'right',
@@ -129,17 +129,17 @@ let missingMessage = 'No biography at the moment. We are very sorry'
 // Data changes
 
 const teamItems = document.querySelectorAll('.team__item');
-const revealImage = document.querySelector('.sidebar__image img');
+const sidebarImage = document.querySelector('.sidebar__image img');
 
-const revealTitle = document.querySelector('.sidebar__title');
-const revealPosition = document.querySelector('.sidebar__position');
-const revealText = document.querySelector('.sidebar__text');
+const sidebarTitle = document.querySelector('.sidebar__title');
+const sidebarPosition = document.querySelector('.sidebar__position');
+const sidebarText = document.querySelector('.sidebar__text');
 
 teamItems.forEach((item) => {
   item.addEventListener('click', () => {
 
     const image = item.querySelector('.team__item-image img');
-    revealImage.src = image.src;
+    sidebarImage.src = image.src;
 
     const name = item.querySelector('.team__item-name');
     const position = item.querySelector('.team__item-position');
@@ -147,12 +147,17 @@ teamItems.forEach((item) => {
     const nameText = name.textContent.trim(); 
     const positionText = position.textContent.trim();
 
-    revealTitle.textContent = nameText;
-    revealPosition.textContent = positionText;
+    if(sidebarTitle){
+      sidebarTitle.textContent = nameText;
+    }
+
+    if(sidebarPosition){
+      sidebarPosition.textContent = positionText;
+    }
 
     const memberData = teamData.find(person => person.name === nameText); 
     if (memberData) {
-      revealText.textContent = memberData.bio;
+      sidebarText.textContent = memberData.text;
     } else {
       revealText.textContent = missingMessage;
     }
